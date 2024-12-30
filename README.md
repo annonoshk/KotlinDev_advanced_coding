7a224e295723ea1c4ec8a83d306e7b8e5114665c
huckk <huck.kim@kangwon.ac.kr>
2024-12-30 20:24:08 +0900

    In the middle of ....

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # KotlinDev_coding_test_execise
 For upgrade my skill 
+
+Advanced Kotiln
+- lambda with receiver
+- DSL
+- Contracts
+- Refrection
+- Custom annotation
+- Scope functions apply, also, let, run, with
+- yaml (map. list config.)
+- coroutins (concourent , async)
+- hash, digest, ciper tech.
+- Predicate
+- File, Path Hierachhies
+- KMP, CMP
+..... TBD
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
-group = "org.crevolika.kotlinAdvanced"
+group = "org.crevolika"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,12 +10,27 @@ repositories {
 }
 
 dependencies {
+    implementation("org.junit.jupiter:junit-jupiter:5.11.4")
     testImplementation(kotlin("test"))
+    implementation(kotlin("test"))
+    testImplementation("io.mockk:mockk:1.13.14")
+    testImplementation ("org.jetbrains.kotlin:kotlin-test:1.5.31")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:2.1.0")
 }
+
 
 tasks.test {
     useJUnitPlatform()
+
+    testLogging  {
+        events ("passed", "skipped", "failed")
+    }
 }
+
 kotlin {
     jvmToolchain(21)
 }
+
+
+
+
